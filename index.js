6aceb3be87270374481772d98602bfb2f0b985b8
Priyanshu <mishrapriyanshu555@gmail.com>
2024-09-17 22:39:47 +0530

    "update"

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "https://jobify-tawny.vercel.app/",
+  origin: "https://jobify-tawny.vercel.app",
   credentials: true,
 };
 
